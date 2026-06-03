96c2055e37cc792e7302c67dbcd1152b0006f77c
Yusuke Sugiyama <114985763+YusukeSugiyamaTG@users.noreply.github.com>
2026-06-03 23:27:43 +0900

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # [{a, 2}, {c, 2}, {e, 1}, {r, 2}]
+        sdict = {}
+        tdict = {}
+        
+        for x in s:
+            if x in sdict:
+                sdict[x] += 1
+            else:
+                sdict[x] = 1
+        
+        for y in t:
+            if y in tdict:
+                tdict[y] += 1
+            else:
+                tdict[y] = 1
+
+        return sdict == tdict
