4da3feff2520c633e76a6624efc334eb970070ec
Yusuke Sugiyama <114985763+YusukeSugiyamaTG@users.noreply.github.com>
2026-06-06 23:43:15 +0900

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        profit = 0
+
+        for p in prices:
+            if p < min_price:
+                min_price = p
+            elif p - min_price > profit:
+                profit = p - min_price
+        return profit
